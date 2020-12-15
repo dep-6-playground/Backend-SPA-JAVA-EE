8594eedde0f3c38d6b05c76b022044c755a8f87f
Deshan Charuka <d.c.0729439631@gmail.com>
2020-12-15 12:52:33 +0530

    Modify item service

--- src/lk/ijse/dep/web/api/ItemServlet.java
+++ src/lk/ijse/dep/web/api/ItemServlet.java
@@ -1,5 +1,8 @@
 package lk.ijse.dep.web.api;
 
+import jakarta.json.bind.Jsonb;
+import jakarta.json.bind.JsonbBuilder;
+import lk.ijse.dep.web.model.Item;
 import org.apache.commons.dbcp2.BasicDataSource;
 
 import javax.servlet.ServletException;
@@ -9,41 +12,125 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.math.BigDecimal;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author : Deshan Charuka <d.c.0729439631@gmail.com>
  * @since : 2020-12-10
  **/
-@WebServlet(name = "ItemServlet",urlPatterns = "/items")
+@WebServlet(name = "ItemServlet", urlPatterns = "/items")
 public class ItemServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-//        resp.addHeader("Access-Control-Allow-Origin","http://localhost:3000");
-        resp.setContentType("application/xml");
-        try (PrintWriter out = resp.getWriter()) {
-            try {
-                BasicDataSource cp = (BasicDataSource) getServletContext().getAttribute("cp");
-                Connection connection = cp.getConnection();
-                Statement stm = connection.createStatement();
-                ResultSet rst = stm.executeQuery("SELECT * FROM Item");
-                out.println("<items>");
+        String code = req.getParameter("code");
 
-                while (rst.next()) {
-                    String code = rst.getString(1);
-                    String des = rst.getString(2);
-                    int qty = rst.getInt(3);
-//                    String qty = rst.getBigDecimal()
+        BasicDataSource cp = (BasicDataSource) getServletContext().getAttribute("cp");
+        resp.setContentType("application/json");
 
-                }
-                connection.close();
-                out.println("</items>");
-            } catch (SQLException e) {
-                e.printStackTrace();
+        try (Connection connection = cp.getConnection()) {
+            PrintWriter out = resp.getWriter();
+            PreparedStatement pstm = connection.prepareStatement("SELECT * FROM Item" + ((code != null) ? " WHERE code=?" : ""));
+            if (code != null) {
+                pstm.setObject(1, code);
             }
+            ResultSet rst = pstm.executeQuery();
+            List<Item> itemList = new ArrayList<>();
+            while (rst.next()) {
+                code = rst.getString(1);
+                String description = rst.getString(2);
+                BigDecimal unitPrice = rst.getBigDecimal(3);
+                int qtyOnHand = rst.getInt(4);
+                itemList.add(new Item(code, description, qtyOnHand, unitPrice));
+            }
+
+            if (code != null && itemList.isEmpty()) {
+                resp.sendError(HttpServletResponse.SC_NOT_FOUND);
+            } else {
+                Jsonb jsonb = JsonbBuilder.create();
+                out.println(jsonb.toJson(itemList));
+                connection.close();
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+            resp.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
         }
     }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("application/json");
+
+        BasicDataSource cp = (BasicDataSource) getServletContext().getAttribute("cp");
+        try (Connection connection = cp.getConnection();) {
+            Jsonb jsonb = JsonbBuilder.create();
+            Item item = jsonb.fromJson(request.getReader(), Item.class);
+
+            /*Validation Logic*/
+            if (item.getCode() == null | item.getDescription() == null || item.getUnitPrice() == null || item.getQtyOnHand() == 0) {
+                response.sendError(HttpServletResponse.SC_BAD_REQUEST);
+                return;
+            }
+
+            if (!item.getCode().matches("I\\d{3}") || item.getDescription().isEmpty()) {
+                response.sendError(HttpServletResponse.SC_BAD_REQUEST);
+                return;
+            }
+
+            PreparedStatement pstm = connection.prepareStatement("INSERT INTO Item VALUES (?,?,?,?)");
+            pstm.setString(1, item.getCode());
+            pstm.setString(2, item.getDescription());
+            pstm.setBigDecimal(3, item.getUnitPrice());
+            pstm.setInt(4,item.getQtyOnHand());
+            if (pstm.executeUpdate() > 0) {
+                response.setStatus(HttpServletResponse.SC_CREATED);
+            } else {
+                response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+            }
+        } catch (SQLIntegrityConstraintViolationException exp) {
+            response.sendError(HttpServletResponse.SC_BAD_REQUEST);
+        } catch (SQLException exp) {
+            response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @Override
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        Todo : Item Update
+    }
+
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        String code = req.getParameter("code");
+        if (code == null || !code.matches("I\\d{3}")) {
+            resp.sendError(HttpServletResponse.SC_BAD_REQUEST);
+            return;
+        }
+
+        BasicDataSource cp = (BasicDataSource) getServletContext().getAttribute("cp");
+        try (Connection connection = cp.getConnection()) {
+            PreparedStatement pstm = connection.prepareStatement("SELECT * FROM Item WHERE code=?");
+            pstm.setObject(1, code);
+            if (pstm.executeQuery().next()) {
+                pstm = connection.prepareStatement("DELETE FROM Item WHERE code=?");
+                pstm.setObject(1, code);
+                boolean success = pstm.executeUpdate() > 0;
+                if (success) {
+                    resp.setStatus(HttpServletResponse.SC_NO_CONTENT);
+                } else {
+                    resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+                }
+            } else {
+                resp.sendError(HttpServletResponse.SC_NOT_FOUND);
+            }
+        } catch (SQLIntegrityConstraintViolationException ex) {
+            resp.sendError(HttpServletResponse.SC_BAD_REQUEST);
+        } catch (SQLException throwables) {
+            resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+            throwables.printStackTrace();
+        }
+    }
+
 }
